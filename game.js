9bf66a10eaa4093d4bd7ab09e691de2e52eb0836
Benjamin Kim <benjaminkim14@gmail.com>
2017-02-05 22:11:20 -0500

    ball bounces!

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var canvas;
 var canvasContent;
 var ballX = 50;
+var ballSpeedX = 5;
 
 window.onload = function(){
 	canvas = document.getElementById("gameCanvas");
@@ -14,14 +15,21 @@ window.onload = function(){
 }
 
 function animateMotion(){
-	ballX = ballX + 5;
+	ballX = ballX + ballSpeedX;
+	if (ballX > canvas.width){
+		ballSpeedX = -ballSpeedX;
+	} else if (ballX < 0){
+		ballSpeedX = -ballSpeedX;
+	}
 }
 
 function drawCanvas(){
-	canvasContent.fillStyle = "black";
-	canvasContent.fillRect(0,0,canvas.width,canvas.height)
-	canvasContent.fillStyle = "white";
-	canvasContent.fillRect(0,200,10,100);
-	canvasContent.fillStyle = "white";
-	canvasContent.fillRect(ballX,100,10,10);
+	drawOnCanvas(0,0,canvas.width,canvas.height,"black"); // create screen 
+	drawOnCanvas(0,200,10,100,"white"); // create left paddle
+	drawOnCanvas(ballX,100,10,10,"white"); //create ball
+}
+
+function drawOnCanvas(leftX,topY, width,height, drawColor){
+	canvasContent.fillStyle = drawColor;
+	canvasContent.fillRect(leftX,topY, width,height);
 }
