0b02074cbf463929e45e538fbac0f711abbf6e9f
Benjamin Kim <benjaminkim14@gmail.com>
2017-02-25 23:32:10 -0500

    basic game function and design complete

--- game.js
+++ game.js
@@ -12,6 +12,9 @@ const PADDLE_HEIGHT = 100;
 
 var p1Score = 0;
 var p2Score = 0;
+const WINNING_SCORE = 3;
+
+var gameOver = false;
 
 
 
@@ -27,6 +30,14 @@ function calcMouse(evt){
 
 }
 
+function startGame(evt){
+	if(gameOver){
+		p1Score = 0;
+		p2Score = 0;
+		gameOver = false;
+	}
+}
+
 window.onload = function(){
 	canvas = document.getElementById("gameCanvas");
 	canvasContent = canvas.getContext("2d");
@@ -37,6 +48,8 @@ window.onload = function(){
 		drawCanvas();
 	}, 1000/fPS);
 
+	canvas.addEventListener('mousedown',startGame)
+
 	canvas.addEventListener('mousemove',
 		function(evt){
 			var mousePos = calcMouse(evt);
@@ -45,6 +58,9 @@ window.onload = function(){
 }
 
 function ballReset(){
+	if (p1Score >= WINNING_SCORE || p2Score >= WINNING_SCORE) {
+		gameOver = true;
+	}
 	ballSpeedX = -ballSpeedX;
 	ballX = canvas.width/2;
 	ballY = canvas.height/2;
@@ -60,6 +76,10 @@ function aiMove(){
 }
 
 function animateMotion(){
+	if (gameOver) {
+		return;
+	}
+
 	aiMove();
 
 	ballX += ballSpeedX;
@@ -67,16 +87,22 @@ function animateMotion(){
 	if (ballX > canvas.width){
 		if (ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
 			ballSpeedX = -ballSpeedX;
+
+			var deltaY = ballY - (paddle2Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.33;
 		} else {
+			p1Score ++; //must be before ball reset
 			ballReset();
-			p1Score ++;
 		}
 	} else if (ballX < 0){
 		if (ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
 			ballSpeedX = -ballSpeedX;
+
+			var deltaY = ballY - (paddle1Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.33;
 		} else {
+			p2Score ++; //must be before ball reset
 			ballReset();
-			p2Score ++;
 		}
 	} else if (ballY > canvas.height){
 		ballSpeedY = -ballSpeedY;
@@ -85,9 +111,29 @@ function animateMotion(){
 	}
 }
 
+function drawNet(){
+	for(var i=0;i < canvas.height; i+=40) {
+		drawOnCanvas(canvas.width/2-1,i,2,20,'white');
+	}
+}
+
 function drawCanvas(){
 	// create screen 
-	drawOnCanvas(0,0,canvas.width,canvas.height,"black"); 
+	drawOnCanvas(0,0,canvas.width,canvas.height,"black");
+
+	if (gameOver) {
+		canvasContent.fillStyle = 'white';
+		if (p1Score >= WINNING_SCORE){
+			console.log("ok");
+			canvasContent.fillText("Player 1 Wins!", 350, 200);
+		} else if (p2Score >= WINNING_SCORE){
+			canvasContent.fillText("Player 2 Wins!", 350, 200);
+		}
+
+		canvasContent.fillText("click to continue", 350, 500);
+		return;
+	} 
+	drawNet();
 	// create left paddle 
 	drawOnCanvas(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,"white");
 	// create right paddle 
