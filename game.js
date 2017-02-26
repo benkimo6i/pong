5ff5b7039f18ae39875b85b56fb5640cd3ab1b66
Benjamin Kim <benjaminkim14@gmail.com>
2017-02-25 22:31:58 -0500

    created my first ai i think

--- game.js
+++ game.js
@@ -5,6 +5,24 @@ var ballY = 50;
 var ballSpeedX = 5;
 var ballSpeedY = 5;
 
+var paddle1Y = 250;
+var paddle2Y = 250;
+const PADDLE_THICKNESS = 10;
+const PADDLE_HEIGHT = 100;
+
+
+function calcMouse(evt){
+	var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	var mouseX = evt.clientX - rect.left - root.scrollLeft;
+	var mouseY = evt.clientY - rect.top - root.scrollTop;
+	return {
+		x:mouseX,
+		y:mouseY
+	};
+
+}
+
 window.onload = function(){
 	canvas = document.getElementById("gameCanvas");
 	canvasContent = canvas.getContext("2d");
@@ -14,15 +32,46 @@ window.onload = function(){
 		animateMotion();
 		drawCanvas();
 	}, 1000/fPS);
+
+	canvas.addEventListener('mousemove',
+		function(evt){
+			var mousePos = calcMouse(evt);
+			paddle1Y = mousePos.y - (PADDLE_HEIGHT/2)
+		})
+}
+
+function ballReset(){
+	ballSpeedX = -ballSpeedX;
+	ballX = canvas.width/2;
+	ballY = canvas.height/2;
+}
+
+function aiMove(){
+	var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
+	if (paddle2YCenter < ballY-35){
+		paddle2Y += 6;
+	} else if (paddle2YCenter > ballY+35){
+		paddle2Y -= 6;
+	}
 }
 
 function animateMotion(){
-	ballX = ballX + ballSpeedX;
-	ballY = ballY + ballSpeedY;
+	aiMove();
+
+	ballX += ballSpeedX;
+	ballY += ballSpeedY;
 	if (ballX > canvas.width){
-		ballSpeedX = -ballSpeedX;
+		if (ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
+			ballSpeedX = -ballSpeedX;
+		} else {
+			ballReset();
+		}
 	} else if (ballX < 0){
-		ballSpeedX = -ballSpeedX;
+		if (ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
+			ballSpeedX = -ballSpeedX;
+		} else {
+			ballReset();
+		}
 	} else if (ballY > canvas.height){
 		ballSpeedY = -ballSpeedY;
 	} else if (ballY < 0){
@@ -34,7 +83,9 @@ function drawCanvas(){
 	// create screen 
 	drawOnCanvas(0,0,canvas.width,canvas.height,"black"); 
 	// create left paddle 
-	drawOnCanvas(0,200,10,100,"white");
+	drawOnCanvas(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,"white");
+	// create right paddle 
+	drawOnCanvas(canvas.width - PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,"white");
 	// create ball
 	drawBall(ballX,ballY,7,"white");
 }
